a3c5b2e2822d2cc5faae7da1eb22573c93d1edbd
Hyacinthe Cartiaux <hyacinthe.cartiaux@free.fr>
2024-04-08 00:38:32 +0200

    ssh public key default value (none)

--- build_openbsd_qcow2.sh
+++ build_openbsd_qcow2.sh
@@ -46,7 +46,7 @@ QEMU_MEM=512m
 DISKLABEL="custom/disklabel"
 INSTALLCONF="custom/install.conf"
 
-SSH_KEY=none
+SSH_KEY_VAL=none
 HTTP_SERVER=10.0.2.2
 DISKLABEL_URL="http://${HTTP_SERVER}/disklabel"
 HOST_NAME="openbsd"
@@ -117,7 +117,7 @@ function build_mirror {
     exec_cmd sed -i "s!\(hostname.=.\).*\$!\1${HOST_NAME}!"      "${PATH_MIRROR}/install.conf"
     exec_cmd sed -i "s!\(HTTP.Server.=/\).*!$/\1${HTTP_SERVER}!" "${PATH_MIRROR}/install.conf"
 
-    SSH_KEY_VAL=$(cat $SSH_KEY)
+    [[ ! -z "$SSH_KEY" ]] && SSH_KEY_VAL=$(cat $SSH_KEY)
     exec_cmd echo "Public ssh key for root account = ${SSH_KEY_VAL}" | tail -n 1 | exec_cmd tee -a "${PATH_MIRROR}/install.conf"
 
     exec_cmd ln -sf "../${DISKLABEL}" "${PATH_MIRROR}/disklabel"
